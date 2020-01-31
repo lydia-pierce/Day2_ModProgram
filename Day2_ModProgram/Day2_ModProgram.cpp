02a1c9166484072a79c520169caca99ffc3674a9
Lydia Pierce <Lydia.Pierce@principia.edu>
2020-01-31 10:55:45 -0600

    Wrote Mod2 program with different algorithm than Mod1

--- Day2_ModProgram/Day2_ModProgram.cpp
+++ Day2_ModProgram/Day2_ModProgram.cpp
@@ -1,9 +1,18 @@
-// Day2_ModProgram.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+/*
+CSCI 182 - Assignment 1
+Day2_ModProgram.cpp
+Manually create a Mod function for use
+
+Lydia Pierce
+v1.0 1/29/2020
+*/
 
 #include <iostream>
 
 
+
+//Get the min value of two input values
 int GetMinValue(int valueA, int valueB)
 {
 	if (valueA < valueB)
@@ -12,16 +21,56 @@ int GetMinValue(int valueA, int valueB)
 		return valueB;
 }
 
+int Mod1(int valA, int valB)
+{
+	if (valA < valB)
+		return valB;
+	else if (valB == 0)
+		return -1;
+	else
+	{
+		while (valA >= valB)
+		{
+			valA = valA - valB;
+		}
+		return valA;
+	}
+}
+
+int Mod2(int value1, int value2)
+{
+	if (value1 < value2)
+		return value2;
+	else if (value2 == 0)
+		return -1;
+	else
+	{
+		int divide = value1 / value2;
+		int remainder = value1 - (value2 * divide);
+		return remainder;
+	}
+}
 
 int main()
 {
-//    std::cout << "Hello World!\n";
+	//    std::cout << "Hello World!\n";
 
-	int a = 400;
-	int b = 220;
+	int a = 19;
+	int b = 19;
 
+	// Find the minimum value and pretty print it to the screen
 	std::cout << "Min Value: " << GetMinValue(a, b) << "\n";
 
+	// Find the remainder when a is divided by b and pretty print it to the screen
+	if (Mod1(a, b) + Mod2(a, b) >= 0)
+	{
+		std::cout << "According to Mod1, the remainder when " << a << " is divided by " << b << " is " << Mod1(a, b) << ".\n";
+		std::cout << "According to Mod2, the remainder when " << a << " is divided by " << b << " is " << Mod2(a, b) << ".\n";
+	}
+	else 
+	{
+		std::cout << "Mod1 and Mod2 are undefined when b is " << b << ".\n";
+	}
 }
 
 
